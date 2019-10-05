67bcfb43f65b14d3e385148453cd365aa6657f1a
gyeomo <canino@naver.com>
2019-10-05 19:18:07 +0900

    version 0.1.0

--- send_file.py
+++ send_file.py
@@ -1,30 +1,48 @@
-import requests
+import numpy as np
+import cv2
 import os
+import time
+from datetime import datetime
+import requests
+import urllib2
+import urllib
 
-path_dir = './test_img/'
+path_dir = './images/'
 url = 'http://203.252.91.45:3000/event/upload'
+prevTime = 0
+cap = cv2.VideoCapture(0)
+idx = 0
+flag = 0
+preTime = time.time()
+while True:
+	curTime = time.time()
+	ret, frame = cap.read()
+	if (curTime - preTime) > 0.4:
+		preTime = time.time()
+		print 'take'
+		now = datetime.now()
+		current_time = str(now.year)+"-"+\
+				str(now.month)+"-"+str(now.day)+"-"+\
+				str(now.hour)+"-"+str(now.minute)+"-"+\
+				str(now.second)+"-"+str(now.microsecond)
+		cv2.imwrite('./images/'+current_time+'.jpg',frame)
+		idx += 1
+		if idx >5 :
+			flag =1
+			idx = 0
+		if flag==1:
+			filename_list = os.listdir(path_dir)
+			file_list = []
+			for each in filename_list:
+				file_obj = []
+				test = path_dir+each
+				file_obj.append('file')
+				file_obj.append(open(path_dir+each,'rb'))
+				file = tuple(file_obj)
+				file_list.append(file)
+				os.remove(path_dir+each)
+			res = requests.post(url, files=file_list)
+			print(res)
+			flag = 0
 
-filename_list = os.listdir(path_dir) # 
-
-print(filename_list) # debug log
-filename_list.remove('.DS_Store') #
-
-file_list = []
-
-# 
-for each in filename_list:
-    file_obj = []
-    file_obj.append('file')
-    file_obj.append(open(path_dir+each, 'rb'))
-    file = tuple(file_obj)
-    file_list.append(file)
-
-print(file_list)
-
-
-res = requests.post(url, files = file_list)
-print(res)
-
-# #test
-# res = requests.get('http://127.0.0.1:3000/users')
-# print(res)
+cap.release()
